22602c46a1f46fdce59774cfba49440c79a01bf8
smilesunho <404orange@naver.com>
2022-06-03 17:25:58 +0900

    codeup

---
+++ python/1714.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+'''
+입력되는 수 N은 0이상의 정수이다.
+입력된 수를 거꾸로 출력한다.
+'''
+
+a=input()
+for i in range(len(a)-1,-1,-1):
+    print(a[i],sep='',end='')
+
+
+# In[ ]:
+
+
+
+
